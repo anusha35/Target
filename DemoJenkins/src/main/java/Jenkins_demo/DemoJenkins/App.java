865c23ef66b10e1615f6690be318235d571374ab
anusha35 <sanusha195@gmail.com>
2016-01-19 15:54:58 +0530

    target

---
+++ DemoJenkins/src/main/java/Jenkins_demo/DemoJenkins/App.java
@@ -0,0 +1,13 @@
+package Jenkins_demo.DemoJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
